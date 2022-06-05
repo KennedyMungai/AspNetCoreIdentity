d4e087868808547c28f1ee8e626aeec6a6b373bc
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-05 11:03:16 +0300

    Created an ApplicationUser model class

---
+++ AspNetCoreIdentity/Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreIdentity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PayrollNumber { get; set; }
+        public string? Region { get; set; }
+        public string? Branch { get; set; }
+        public DateTime DateAndTimeReported { get; set; }
+    }
+}
